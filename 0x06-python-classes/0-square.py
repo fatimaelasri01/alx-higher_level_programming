3988b8b058264ba7ee09d4f5fa61bb3b139b1526
fatimaelasri01 <fatimaelassri041@gmail.com>
2023-12-19 16:29:55 -0800

    update

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Represents a basic square with no specific attributes or methods.
+"""
+
+
+class Square:
+    """
+    A simple class representing a square with no defined attributes or methods.
+    """
+    pass
