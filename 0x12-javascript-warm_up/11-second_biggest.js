14b18ca3f871e4328505a316fbd064e1d8e5f0c5
fatimaelasri01 <fatimaelassri041@gmail.com>
2024-03-11 19:55:56 -0700

    up

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (process.argv.length <= 3) {
+	console.log('0');
+} else {
+	const arry = process.argv.slice(2).map(Number);
+	const second = arry.sort(function (a, b) { return b - a; })[1];
+	console.log(second);
+}
