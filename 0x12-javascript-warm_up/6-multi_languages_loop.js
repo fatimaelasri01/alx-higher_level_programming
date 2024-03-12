197f5267a52daeb526d7aa073d4ee54c43fe2a1d
fatimaelasri01 <fatimaelassri041@gmail.com>
2024-03-12 15:32:05 +0000

    up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const arrayOfStrings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < arrayOfStrings; i++) {
+for (let i = 0; i < arrayOfStrings.length; i++) {
   console.log(arrayOfStrings[i]);
 }
