856920f406d7085b15e7a7662e5e060ec9dd07ef
fatimaelasri01 <fatimaelassri041@gmail.com>
2024-03-13 22:27:08 +0000

    up

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,10 +3,10 @@ const a = Number(process.argv[2]);
 const b = Number(process.argv[3]);
 
 function add (a, b) {
-	console.log(a + b);
+  console.log(a + b);
 }
 if (isNaN(a) || isNaN(b)) {
-	console.log('NaN');
+  console.log('NaN');
 } else {
-	add(a, b);
+  add(a, b);
 }
