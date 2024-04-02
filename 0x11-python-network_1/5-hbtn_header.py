5e010389bf30399afba012c42105225dd0c0565c
fatimaelasri01 <fatimaelassri041@gmail.com>
2024-04-01 19:24:09 -0700

    network

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -8,10 +8,7 @@ in the response header
 
 import requests
 import sys
-
 if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
-    
     print(response.headers.get('X-Request-Id'))
-
